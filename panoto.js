585e960e73b603205cb470f5492f3632c10e7baf
Dedsec-Xu <xld1998615@vip.qq.com>
2020-09-18 04:44:41 +0800

    Update panoto.js

--- panoto.js
+++ panoto.js
@@ -1,12 +1,12 @@
 javascript:(function()
 {
-if(window.location.href.indexOf("panopto.com") < 1) 
-{ 
-	alert("No video"); 
-	exit(); 
-}
-var metas = document.getElementsByTagName('meta'); 
-for (var i=0; i<metas.length; i++) 
+	if(window.location.href.indexOf("panopto.com") < 1) 
+	{ 
+		alert("No video"); 
+		exit(); 
+	}
+	var metas = document.getElementsByTagName('meta'); 
+	for (var i=0; i<metas.length; i++) 
 	if (metas[i].getAttribute("name") == "twitter:player:stream") 
 	{ 
 		alert("The video adress is: " + metas[i].getAttribute("content").split('?')[0]);
